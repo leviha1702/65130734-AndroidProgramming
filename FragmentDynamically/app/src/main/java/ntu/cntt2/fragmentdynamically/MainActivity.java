51bc66aad053acc62b5eb772b9c7a1048acbedea
LÊ VIỆT HÀ <levietha170205@gmail.com>
2026-04-06 16:24:04 +0700

    BaiTH12

---
+++ FragmentDynamically/app/src/main/java/ntu/cntt2/fragmentdynamically/MainActivity.java
@@ -0,0 +1,26 @@
+package ntu.cntt2.fragmentdynamically;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fameContentHoder, new ContentFragment())
+                .add(R.id.frameFooterHoder, new FooterFragment())
+                .commit();
+    }
+}
